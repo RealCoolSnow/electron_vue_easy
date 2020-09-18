f49bf0f9b8df1be00dce427cfa395ef3129a2d3f
CoolSnow <coolsnow2020@gmail.com>
2020-09-18 11:19:46 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
+  extends: [
     // 检测vue的
     'plugin:vue/essential',
     // ESlint标准的检测
@@ -12,13 +12,13 @@ module.exports = {
   rules: {
     'no-console': 'off',
     'no-debugger': 'off',
-    'semi': ['error', 'never'],  // 强制分号
-    'indent': ['error', 2], // 2个空格
-    'camelcase': 0, // 不强制_形式，或者驼峰
+    semi: ['error', 'never'], // 强制分号
+    indent: ['error', 2], // 2个空格
+    camelcase: 0, // 不强制_形式，或者驼峰
     'vue/html-indent': ['error', 2], // vue中2个空格
-    'vue/require-default-prop': 'off',  // prop的值设置默认值（后期关闭该规则）
+    'vue/require-default-prop': 'off', // prop的值设置默认值（后期关闭该规则）
     'vue/require-prop-type-constructor': 'off', // 关闭prop强制类型设置（后期关闭该规则）
-    'vue/require-prop-types': 'off',  // 关闭prop强制类型设置（后期关闭该规则）
+    'vue/require-prop-types': 'off', // 关闭prop强制类型设置（后期关闭该规则）
     'vue/prop-name-casing': 'off' // prop的属性名可以为驼峰，也可以_表示
   },
   parserOptions: {
